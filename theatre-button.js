df17e56d3ce93b40c7b802c59000b0d72a470d9f
Noikstrax <noikstrax@tuta.io>
2025-01-26 22:47:01 +0300

    stream support and svg change on click

--- theatre-button.js
+++ theatre-button.js
@@ -1,66 +1,115 @@
+const openedButtonSvg = "M4 3a2 2 0 00-2 2v10a2 2 0 002 2h7V3H4zM16 3h-3v14h3a2 2 0 002-2V5a2 2 0 00-2-2z";
+const notOpenedButtonSvg = "M2 15V5a2 2 0 012-2h12a2 2 0 012 2v10a2 2 0 01-2 2H4a2 2 0 01-2-2zm2 0V5h7v10H4zm9 0h3V5h-3v10z";
+
+
 
 
 
 
 
 //live
-//play-wrapper
- 
-checkIsLoadedPlayer();    
-function checkIsLoadedPlayer() {
+const checkIsLoadedPlayer = () => {
     if (!document.body.getElementsByClassName('live')[0]) {
-        setTimeout(() => {
-            checkIsLoadedPlayer();
-        }, 500);
+        setTimeout(checkIsLoadedPlayer, 500);
     } else {
         setTimeout(() => {
             addButton();
             addPlayerClickEvent();
-        }, 500);
+        }, 300);
     }
-}
+};
 
-
-
-function addPlayerClickEvent() {
-    if (document.querySelectorAll('.player-poster.clickable')[0]) {
-        let playButton = document.querySelectorAll('.player-poster.clickable')[0];
-        playButton.addEventListener('click', function () {
-            if(!document.body.getElementsByClassName('player-button theatre-button')[0]) {
+const addPlayerClickEvent = () => {
+    const playButton = document.querySelector('.player-poster.clickable');
+    const playStopButton = document.querySelector('.player-button.pause-play-button');
+    if (playButton) {
+        playButton.addEventListener('click', () => {
+            if (!document.body.getElementsByClassName('player-button theatre-button')[0]) {
                 addButton();
-            } else {
-                return 0;
             }
+        });
 
-        })
-        
-
-
-    } else {
-        return 0;
+        playStopButton.addEventListener('click', () => {
+            if (!document.body.getElementsByClassName('player-button theatre-button')[0]) {
+                addButton();
+            }
+        });
     }
-}
-
+};
 
 let isTheaterMode = false;
 
+const toggleTheaterMode = () => {
+    const elementsToToggle = [
+        'app-load-more',
+        'app-header',
+        'app-content-feed-block',
+        'app-watcher-player-bottom-panel',
+        'app-watcher-feed',
+        'canvas'
+    ];
 
-    function addButton() {
+    elementsToToggle.forEach(tag => {
+        const element = document.body.getElementsByTagName(tag);
+        if (element.length > 1) {
+            for (let i = 0; i < element.length; i++) {
+                element[i].hidden = !element[i].hidden;
+            }
+        } else {
+            element[0].hidden = !element[0].hidden;
 
-    
-    
+        }
+    });
+
+    const mainElement = document.querySelector("body > app-root > app-watcher-layout > div > main");
+    const sidebarElement = document.querySelector("[appwatchersidebardisplay]");
+    const playerWrapper = document.body.getElementsByClassName('player-area__player-wrapper')[0];
+    const feedElement = document.body.getElementsByTagName('app-watcher-feed')[0];
+
+    if (!isTheaterMode) {
+        mainElement.style.margin = 0;
+        sidebarElement.style.padding = 0;
+        playerWrapper.style.height = "100vh";
+        feedElement.style.margin = 0;
+        feedElement.style.padding = 0;
+        document.body.getElementsByClassName('player-button theatre-button')[0].children[0].textContent = "В нормальный режим";
+        document.body.getElementsByClassName('player-button theatre-button')[0].children[1].children[0].setAttribute('d', openedButtonSvg);
+    } else {
+        mainElement.removeAttribute("style");
+        sidebarElement.removeAttribute("style");
+        playerWrapper.removeAttribute("style");
+        feedElement.removeAttribute("style");
+        document.body.getElementsByClassName('player-button theatre-button')[0].children[0].textContent = "Театральный режим";
+        document.body.getElementsByClassName('player-button theatre-button')[0].children[1].children[0].setAttribute('d', notOpenedButtonSvg);
+    }
+
+    isTheaterMode = !isTheaterMode;
+};
+
+const addButton = () => {
+
+    let fullscreenButton = document.querySelectorAll('.player-button.fullscreen-button')[0];
+    if (!fullscreenButton.hasEventListener) {
+        fullscreenButton.addEventListener('click', () => {
+            if (isTheaterMode) {
+                toggleTheaterMode();
+            }
+            document.body.getElementsByClassName('player-button theatre-button')[0].parentElement.classList.toggle('hidden');
+            
+        });
+        fullscreenButton.hasEventListener = true;
+    }
     const div = document.createElement('div');
     div.className = 'buttons-wrapper';
+
     const button = document.createElement('button');
     button.className = 'player-button theatre-button';
     button.type = 'button';
-    
-    // Создаем тултип
+
     const tooltip = document.createElement('div');
     tooltip.className = 'tooltip tooltip-up tooltip-align-right';
     tooltip.textContent = 'Театральный режим';
-    
-    // Создаем SVG элемент (если нужно)
+
     const svgNS = 'http://www.w3.org/2000/svg';
     const svg = document.createElementNS(svgNS, 'svg');
     svg.setAttribute('width', '24');
@@ -68,63 +117,25 @@ let isTheaterMode = false;
     svg.setAttribute('viewBox', '0 0 20 20');
     svg.setAttribute('fill', 'none');
     svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-    
-    // Создаем path для SVG
+
     const path = document.createElementNS(svgNS, 'path');
     path.setAttribute('fill-rule', 'evenodd');
     path.setAttribute('clip-rule', 'evenodd');
-    path.setAttribute('d', 'M2 15V5a2 2 0 012-2h12a2 2 0 012 2v10a2 2 0 01-2 2H4a2 2 0 01-2-2zm2 0V5h7v10H4zm9 0h3V5h-3v10z');
+    path.setAttribute('d', notOpenedButtonSvg);
     path.setAttribute('fill', '#fff');
-    
-    // Добавляем path в SVG
+
     svg.appendChild(path);
-    
-    
-    // Добавляем тултип и SVG в кнопку
     button.appendChild(tooltip);
     button.appendChild(svg);
-    
-    //Добавляем кнопку в div
-    
     div.appendChild(button);
-    
-    // Добавляем div в контейнер
+
     const buttonsWrapper = document.querySelector('.buttons-right');
-    buttonsWrapper.appendChild(div);
-
-    button.addEventListener("click", function() {
-
-        if (!isTheaterMode) {
-
-        isTheaterMode = !isTheaterMode;
-        document.body.getElementsByTagName('app-load-more')[0].hidden = !document.body.getElementsByTagName('app-load-more')[0].hidden;
-        document.body.getElementsByTagName('app-header')[0].hidden = !document.body.getElementsByTagName('app-header')[0].hidden;
-        document.body.getElementsByTagName('app-content-feed-block')[0].hidden = 
-        !document.body.getElementsByTagName('app-content-feed-block')[0].hidden;
-        document.body.getElementsByTagName('app-watcher-player-bottom-panel')[0].hidden = 
-        !document.body.getElementsByTagName('app-watcher-player-bottom-panel')[0].hidden;
-        document.querySelector("body > app-root > app-watcher-layout > div > main").style.margin = 0
-        document.querySelector("[appwatchersidebardisplay]").style.padding = 0;
-        document.body.getElementsByClassName('player-area__player-wrapper')[0].style.height = "100vh";
-        document.body.getElementsByTagName('app-watcher-feed')[0].style.margin = 0;
-        document.body.getElementsByTagName('app-watcher-feed')[0].style.padding = 0;
-
-        } else {
-
-        isTheaterMode = !isTheaterMode;
-        document.body.getElementsByTagName('app-load-more')[0].hidden = !document.body.getElementsByTagName('app-load-more')[0].hidden;
-        document.body.getElementsByTagName('app-header')[0].hidden = !document.body.getElementsByTagName('app-header')[0].hidden;
-        document.body.getElementsByTagName('app-content-feed-block')[0].hidden = 
-        !document.body.getElementsByTagName('app-content-feed-block')[0].hidden;
-        document.body.getElementsByTagName('app-watcher-player-bottom-panel')[0].hidden = 
-        !document.body.getElementsByTagName('app-watcher-player-bottom-panel')[0].hidden;
-        document.querySelector("body > app-root > app-watcher-layout > div > main").removeAttribute("style");
-        document.querySelector("[appwatchersidebardisplay]").removeAttribute("style");
-        document.body.getElementsByClassName('player-area__player-wrapper')[0].removeAttribute("style");
-        document.body.getElementsByTagName('app-watcher-feed')[0].removeAttribute("style");
-
-        }
-        
-    });
-    
+    const beforeElement = document.querySelector('.player-button.fullscreen-button').parentElement;
+    if (buttonsWrapper) {
+        buttonsWrapper.insertBefore(div, beforeElement);
     }
+
+    button.addEventListener("click", toggleTheaterMode);
+};
+
+checkIsLoadedPlayer();
