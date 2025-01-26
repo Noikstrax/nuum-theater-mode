3d5e8671d118ca87c24e6280f00b699916c9da8c
Noikstrax <noikstrax@tuta.io>
2025-01-27 01:27:51 +0300

    fix double add button

--- theatre-button.js
+++ theatre-button.js
@@ -1,23 +1,27 @@
 const openedButtonSvg = "M4 3a2 2 0 00-2 2v10a2 2 0 002 2h7V3H4zM16 3h-3v14h3a2 2 0 002-2V5a2 2 0 00-2-2z";
 const notOpenedButtonSvg = "M2 15V5a2 2 0 012-2h12a2 2 0 012 2v10a2 2 0 01-2 2H4a2 2 0 01-2-2zm2 0V5h7v10H4zm9 0h3V5h-3v10z";
 let isTheaterMode = false;
-
+let fullscreenButtonHandled = false;
+let isPlayerHandled = false; 
+let isButtonAdded = false;
 
 const checkIsLoadedPlayer = () => {
-    if (!document.body.getElementsByTagName('video')[0]) {
+    const videoElement = document.body.getElementsByTagName('video')[0];
+    
+    if (!videoElement) {
         setTimeout(checkIsLoadedPlayer, 100);
-    } else {
-        let videoElement = document.body.getElementsByTagName('video')[0];
-        videoElement.addEventListener('play', function() {
-            if (!document.body.getElementsByClassName('player-button theatre-button')[0]) {
+    } else if (!isPlayerHandled) {
+        videoElement.addEventListener('play', function onPlay() {
+            if (!isButtonAdded && !document.body.getElementsByClassName('player-button theatre-button')[0]) {
                 setTimeout(() => {
-                addButton()
-
+                    addButton(); 
+                    isButtonAdded = true; 
+                    setTimeout(checkButton, 5000);
                 }, 500);
-                
             }
-            
-          });
+        });
+
+        isPlayerHandled = true; 
     }
 };
 
@@ -74,53 +78,64 @@ const toggleTheaterMode = () => {
 
 
 const addButton = () => {
+    if (!document.body.getElementsByClassName('player-button theatre-button')[0]) {
+        let fullscreenButton = document.querySelectorAll('.player-button.fullscreen-button')[0];
+        if (!fullscreenButtonHandled) {
+            fullscreenButton.addEventListener('click', () => {
+                document.body.getElementsByClassName('player-button theatre-button')[0].parentElement.classList.toggle('hidden');
+                
+            });
+            fullscreenButtonHandled = true;
+        }
+        const div = document.createElement('div');
+        div.className = 'buttons-wrapper';
+    
+        const button = document.createElement('button');
+        button.className = 'player-button theatre-button';
+        button.type = 'button';
+    
+        const tooltip = document.createElement('div');
+        tooltip.className = 'tooltip tooltip-up tooltip-align-right';
+        tooltip.textContent = 'Театральный режим';
+    
+        const svgNS = 'http://www.w3.org/2000/svg';
+        const svg = document.createElementNS(svgNS, 'svg');
+        svg.setAttribute('width', '24');
+        svg.setAttribute('height', '24');
+        svg.setAttribute('viewBox', '0 0 20 20');
+        svg.setAttribute('fill', 'none');
+        svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+    
+        const path = document.createElementNS(svgNS, 'path');
+        path.setAttribute('fill-rule', 'evenodd');
+        path.setAttribute('clip-rule', 'evenodd');
+        path.setAttribute('d', notOpenedButtonSvg);
+        path.setAttribute('fill', '#fff');
+    
+        svg.appendChild(path);
+        button.appendChild(tooltip);
+        button.appendChild(svg);
+        div.appendChild(button);
+    
+        const buttonsWrapper = document.querySelector('.buttons-right');
+        const beforeElement = document.querySelector('.player-button.fullscreen-button').parentElement;
+        if (buttonsWrapper) {
+            buttonsWrapper.insertBefore(div, beforeElement);
+        }
+    
+        button.addEventListener("click", toggleTheaterMode);
 
-    let fullscreenButton = document.querySelectorAll('.player-button.fullscreen-button')[0];
-    if (!fullscreenButton.hasEventListener) {
-        fullscreenButton.addEventListener('click', () => {
-            document.body.getElementsByClassName('player-button theatre-button')[0].parentElement.classList.toggle('hidden');
-            
-        });
-        fullscreenButton.hasEventListener = true;
     }
-    const div = document.createElement('div');
-    div.className = 'buttons-wrapper';
 
-    const button = document.createElement('button');
-    button.className = 'player-button theatre-button';
-    button.type = 'button';
-
-    const tooltip = document.createElement('div');
-    tooltip.className = 'tooltip tooltip-up tooltip-align-right';
-    tooltip.textContent = 'Театральный режим';
-
-    const svgNS = 'http://www.w3.org/2000/svg';
-    const svg = document.createElementNS(svgNS, 'svg');
-    svg.setAttribute('width', '24');
-    svg.setAttribute('height', '24');
-    svg.setAttribute('viewBox', '0 0 20 20');
-    svg.setAttribute('fill', 'none');
-    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-
-    const path = document.createElementNS(svgNS, 'path');
-    path.setAttribute('fill-rule', 'evenodd');
-    path.setAttribute('clip-rule', 'evenodd');
-    path.setAttribute('d', notOpenedButtonSvg);
-    path.setAttribute('fill', '#fff');
-
-    svg.appendChild(path);
-    button.appendChild(tooltip);
-    button.appendChild(svg);
-    div.appendChild(button);
-
-    const buttonsWrapper = document.querySelector('.buttons-right');
-    const beforeElement = document.querySelector('.player-button.fullscreen-button').parentElement;
-    if (buttonsWrapper) {
-        buttonsWrapper.insertBefore(div, beforeElement);
-    }
-
-    button.addEventListener("click", toggleTheaterMode);
+   
 };
+
+function checkButton() {
+    if (!document.body.getElementsByClassName('player-button theatre-button')[0]) {
+        addButton();
+    }
+}
 
 
 checkIsLoadedPlayer();
+
